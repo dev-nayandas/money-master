8756ff192dfc781736057c60a2fe3458e0bf1f64
dev-nayandas <rich.nayan@gmail.com>
2022-02-17 01:48:55 +0600

    total expenses and balance calculation done

--- app.js
+++ app.js
@@ -33,5 +33,21 @@ document.getElementById('calculation').addEventListener('click', function () {
 
     //console.log(balanceAmount);
 });
+document.getElementById('saving-btn').addEventListener('click', function(){
+    
+
+    const savingField = document.getElementById('saving-field').value;
+    const savingAmount =parseFloat(savingField);
+
+    const savingTotal = document.getElementById('saving-value');
+    const saving = savingTotal.innerText;
+
+    const monthlyIncome = document.getElementById('monthly-income').value;
+    const monthlyEarn = parseFloat(monthlyIncome);
+
+
+    const savingFinalAmount = (20*100) / monthlyEarn
+    console.log(savingAmount)
+})
 
 
